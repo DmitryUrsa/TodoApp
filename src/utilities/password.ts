e6e4b1d1fe09fef951e3393ca2ab7a1f40382ec7
The Urz <r2d2btw@gmail.com>
2022-08-03 20:16:04 +0500

    Compare passwords

--- src/utilities/password.ts
+++ src/utilities/password.ts
@@ -3,5 +3,8 @@ import bcrypt from "bcryptjs"
 function hashPassword(password: string) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10))
 }
+function comparePassword(password: string, hash: string) {
+  return bcrypt.compareSync(password, hash)
+}
 
-export { hashPassword }
+export { hashPassword, comparePassword }
